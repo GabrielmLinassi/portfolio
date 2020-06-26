942cd6bf5b197543a4d8baf83da2829aecb10ab4
Gabriel Linassi <gabrielm.linassi@hotmail.com>
2020-06-26 12:11:42 -0300

    Redesign of skills section to use progressbar

--- src/js/index.js
+++ src/js/index.js
@@ -32,3 +32,23 @@ $(window).scroll(() => {
     $returnTop.fadeOut(200);
   }
 });
+
+// ======== Skillbar animation ===
+function SKILLBAR_ANIME() {
+  const SKILLBAR = document.querySelectorAll(".skillbar");
+
+  SKILLBAR.forEach((el) => {
+    const SKILLBAR_HEADWAY = el.querySelector(".skillbar__headway");
+    const SKILLBAR_HEADER = el.querySelector(".skillbar__header");
+    const SKILLBAR_VALUE = SKILLBAR_HEADWAY.dataset.skillbarValue;
+    const SKILLBAR_NAME = SKILLBAR_HEADWAY.dataset.skillbarLabel;
+    const SKILLBAR_HEADWAY_NAME = el.querySelector(".skillbar__label");
+    const SKILLBAR_HEADWAY_COUNTER = el.querySelector(".skillbar__counter");
+
+    SKILLBAR_HEADWAY_NAME.textContent = `${SKILLBAR_NAME}`;
+    SKILLBAR_HEADWAY_COUNTER.textContent = `${SKILLBAR_VALUE}%`;
+    SKILLBAR_HEADWAY.style.width = `${SKILLBAR_VALUE}%`;
+  });
+}
+
+document.addEventListener("DOMContentLoaded", SKILLBAR_ANIME);
