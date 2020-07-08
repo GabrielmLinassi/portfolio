714090f1048f76d8b784422368e692eb4119cdd4
Gabriel Linassi <gabrielm.linassi@hotmail.com>
2020-07-07 23:40:32 -0300

    Updates

--- js/feature-flag.js
+++
@@ -1,1 +0,0 @@
-function featureIsActive(e){return{"opt-in":userHasOptedIntoFeature(e),active:!0,inactive:!1}[getFeatureStatusByFeatureName(e)]}const getFeatureStatusByFeatureName=e=>{return{"new-contact-page":"active"}[e]||"inactive"};function userHasOptedIntoFeature(e){}
